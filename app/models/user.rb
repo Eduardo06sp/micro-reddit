8d9bf8362cb7437bc9b7b6113ce52f63658c7c1c
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-01-01 00:39:53 -0800

    feat: add User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  validates :user_name,
+            length: { in: 4..12 },
+            presence: true,
+            uniqueness: true
+  validates :password,
+            length: { in: 6..12 },
+            presence: true
 end
